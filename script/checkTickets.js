227deaed0fde7716bd934b0b45f02c539932a602
scott <tong233@users.noreply.github.com>
2026-07-25 13:11:26 +0800

    feat: 无头浏览器更换为playwright

--- script/checkTickets.js
+++ script/checkTickets.js
@@ -3,10 +3,7 @@
  */
 const CronJob = require("cron").CronJob;
 const notifier = require("node-notifier");
-const puppeteer = require("puppeteer-extra");
-const StealthPlugin = require("puppeteer-extra-plugin-stealth");
-
-puppeteer.use(StealthPlugin());
+const { chromium } = require("playwright");
 
 const CHECK_URL =
   "https://bawtt.ydmap.cn/booking/schedule/102824?salesItemId=100908";
@@ -19,25 +16,27 @@ function delay(ms) {
 }
 
 async function checkTickets() {
-  const browser = await puppeteer.launch({
-    headless: "new",
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  });
-  const page = await browser.newPage();
-  await page.setJavaScriptEnabled(true);
-  console.log("开始查询页面");
-  await page.goto(CHECK_URL);
+  let browser;
 
   try {
+    browser = await chromium.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+    const page = await browser.newPage({ javaScriptEnabled: true });
+    console.log("开始查询页面");
+    await page.goto(CHECK_URL);
+  await page.screenshot({
+      path: "screenshot.png", // 保存路径
+      fullPage: true, // 截取整个可滚动页面
+    });
     // 等待页面加载完成
     await page.waitForSelector(".schedule-table-compact");
-    // await page.screenshot({
-    //   path: "screenshot.png", // 保存路径
-    //   fullPage: true, // 截取整个可滚动页面
-    // });
+  
 
     // 获取所有时间段信息
     const timeCells = await page.$$(".schedule-table-compact tr");
+    console.log(timeCells);
     for (let index = 0; index < timeCells.length; index++) {
       const cell = timeCells[index];
 
@@ -58,16 +57,17 @@ async function checkTickets() {
             message: ticketText,
             wait: true,
           });
-          return
+          return;
         }
       }
     }
-     console.log(`[${new Date().toLocaleTimeString()}]`, `约满了`);
-
-    await browser.close();
+    console.log(`[${new Date().toLocaleTimeString()}]`, `约满了`);
   } catch (error) {
     console.error("出现错误：", error);
-    await browser.close();
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
   }
 }
 
